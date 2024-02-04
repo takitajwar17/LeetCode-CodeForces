39e5b379b339c059498abee8b63184a7b36084ea
takitajwar17 <tajwaruzzaman@iut-dhaka.edu>
2024-02-04 23:18:06 +0600

    input updated

--- 217. Contains Duplicate.py
+++ 217. Contains Duplicate.py
@@ -1,8 +1,16 @@
-def containsDuplicate(self, nums: List[int]) -> bool:
-    storage = set()
-    for num in nums:
-        if num in storage:
-            return True
-        else:
-            storage.add(num)
-    return False
+from typing import List
+
+class Solution:
+    def containsDuplicate(self, nums: List[int]) -> bool:
+        storage = set()
+        for num in nums:
+            if num in storage:
+                return True
+            else:
+                storage.add(num)
+        return False
+
+# Example usage:
+solution = Solution()
+nums = [1, 2, 3, 4, 1]
+print(solution.containsDuplicate(nums))
